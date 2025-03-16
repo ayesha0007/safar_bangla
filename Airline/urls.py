c05eeaf32a515d3cfe215df76beff90cee689c73
ayesha0007 <tasnisa0007@gmail.com>
2025-03-16 22:41:03 +0600

    final-error

--- Airline/urls.py
+++ Airline/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
 # from .views import get_flights
-from .views import get_biman_flights
+from .views import get_flights
 
 urlpatterns = [
     # path('reserve/<str:det_from>TO<str:det_to>',views.flight_booking, name="flight"),
     # path('flights/', get_flights, name='get_flights'),
-    path('biman-flights/', get_biman_flights, name="biman_flights")
+    path('biman-flights/', get_flights, name="biman_flights")
 ]
 
